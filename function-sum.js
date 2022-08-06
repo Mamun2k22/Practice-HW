fdbd027c12e1456010f8694e625d774efe97636b
Mamun2k22 <mamun180037@gmail.com>
2022-08-06 19:43:17 +0600

    Sum equal to using on Function

---
+++ function-sum.js
@@ -0,0 +1,9 @@
+function sumnumber(num1,num2,num3){
+    var result = num1 + num2 + num3;
+    return result;
+}
+ var number1 = 100;
+ var number2 = 500;
+ var number3 = 400;
+ var sum = sumnumber(number1,number2,number3);
+ console.log (sum);
